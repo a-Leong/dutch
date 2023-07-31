d3d50d5c2ca7d75f5ac7a28e9369231e0a11b2ac
Alex Leong <alexleong7@gmail.com>
2023-07-30 17:26:25 -0700

    move GCP creds to fly secrets, .env

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 import { WebSocketServer } from "ws";
-import { applicationDefault, initializeApp } from "firebase-admin/app";
+import { initializeApp } from "firebase-admin/app";
 import { getFirestore, FieldValue } from "firebase-admin/firestore";
 import { getDatabase, ServerValue } from "firebase-admin/database";
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (process.env.GCP_SA_KEY === undefined) {
+  throw new Error("Firebase admin SDK credentials not found in ENV.");
+}
 
 const app = initializeApp({
-  credential: applicationDefault(),
+  credential: admin.credential.cert(JSON.parse(process.env.GCP_SA_KEY)),
   databaseURL: "https://dutch-cards-default-rtdb.firebaseio.com",
 });
 const database = getDatabase(app);
